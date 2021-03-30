b5b53ff1a13e752df3e1b6c0a7fe2bcfdeef2aa6
zogorn <king.zogorn@gmail.com>
2021-03-30 13:39:42 +0200

    add header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="nav">
+        <div className="nav-menus">
+          <div className="nav-brand">
+            <a className="nav-brand-logo" href="/">
+              Instagram
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
